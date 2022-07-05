529dcfe729f24f4bf20a490336f3453b3dfdc8d4
Manav Kalra <hellomanav>
2022-07-05 12:35:47 +0000

    Initial commit

---
+++ src/main/groovy/com/manav/slackbot/Application.groovy
@@ -0,0 +1,11 @@
+package com.manav.slackbot
+
+import io.micronaut.runtime.Micronaut
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class Application {
+    static void main(String[] args) {
+        Micronaut.run(Application, args)
+    }
+}
---
+++ src/test/groovy/com/manav/slackbot/SlackBotTest.groovy
@@ -0,0 +1,19 @@
+package com.manav.slackbot
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class SlackBotTest {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    @Test
+    void testItWorks() {
+        assert application.running == true
+    }
+
+}
